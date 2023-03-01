63bea253c022722b47172d62ea1a4af3e5630580
Boran Gökden <90551725+borangokden@users.noreply.github.com>
2023-03-01 18:43:40 +0300

    Update sil.js

--- commands/sil.js
+++ commands/sil.js
@@ -2,11 +2,11 @@ const {EmbedBuilder} = require("discord.js");
 const config = require("../config.js")
 
 exports.run = async (client, message, args) => {
-
+  
+if (!message.member.permissions.has("MANAGE_MESSAGE")) return message.channel.send("Yetkiniz yetersiz kaldı, mesajları yönet yetkisine sahip olmanız gerekmekte.");
 const bgmiktar = args[0]
 if(!bgmiktar) return message.reply("Öncelikle bir miktar belirtin!")
 message.channel.bulkDelete(bgmiktar).catch(err => message.reply("Beklenmedik bir hata oluştu!"))
-if (!message.member.permissions.has("MANAGE_MESSAGE")) return message.channel.send("Yetkiniz yetersiz kaldı, mesajları yönet yetkisine sahip olmanız gerekmekte.");
 message.channel.send({content: `Başarıyla ${bgmiktar} miktar mesaj silindi.`})
 
 };
